04f45595cc5b8d8d8be2047ce1fd0b629693b236
GaelArana <141584133+GaelArana@users.noreply.github.com>
2023-08-07 10:51:26 -0600

    Add files via upload

---
+++ piedra papel o tijera/main.js
@@ -0,0 +1,116 @@
+let puntosUsuario = 0;
+let puntosPC = 0;
+
+const reglas = document.getElementById("reglas");
+const historia = document.getElementById("historia");
+
+let instrucciones = document.querySelector("#instrucciones");
+let contenedorPuntosUsuario = document.querySelector("#puntos-usuario");
+let contenedorPuntosPC = document.querySelector("#puntos-computadora");
+let mensaje = document.querySelector("#mensaje");
+let contenedorGanaPunto = document.querySelector("#gana-punto");
+let elegiTuArma = document.querySelector("#elegi-tu-arma");
+
+let contenedorEleccionUsuario = document.querySelector("#eleccion-usuario");
+let contenedorEleccionPC = document.querySelector("#eleccion-computadora");
+
+let botonesArmas = document.querySelectorAll(".arma");
+botonesArmas.forEach(boton => {
+    boton.addEventListener("click", iniciarTurno);
+});
+
+function iniciarTurno(e) {
+    
+    let eleccionPC = Math.floor(Math.random() * 3);
+    let eleccionUsuario = e.currentTarget.id;
+
+    if (eleccionPC === 0) {
+        eleccionPC = "piedra🪨";
+    } else if (eleccionPC === 1) {
+        eleccionPC = "papel📋"
+    } else if (eleccionPC === 2) {
+        eleccionPC = "tijera✂️"
+    }
+
+    if (
+        (eleccionUsuario === "piedra🪨" && eleccionPC === "tijera✂️") ||
+        (eleccionUsuario === "tijera✂️" && eleccionPC === "papel📋") ||
+        (eleccionUsuario === "papel📋" && eleccionPC === "piedra🪨")
+    ) {
+        ganaUsuario();
+    } else if (
+        (eleccionPC === "piedra🪨" && eleccionUsuario === "tijera✂️") ||
+        (eleccionPC === "tijera✂️" && eleccionUsuario === "papel📋") ||
+        (eleccionPC === "papel📋" && eleccionUsuario === "piedra🪨")
+    ) {
+        ganaPC();
+    } else {
+        empate();
+    }
+
+    mensaje.classList.remove("disabled");
+    contenedorEleccionUsuario.innerText = eleccionUsuario;
+    contenedorEleccionPC.innerText = eleccionPC;
+
+    if (puntosUsuario === 5 || puntosPC === 5) {
+
+        if (puntosUsuario === 5) {
+            instrucciones.innerText = "¡Ganaste el juego!"
+        }
+
+        if (puntosPC === 5) {
+            instrucciones.innerText = "¡La computadora ganó el juego!"
+        }
+
+        elegiTuArma.classList.add("disabled");
+        reiniciar.classList.remove("disabled");
+        reiniciar.addEventListener("click", reiniciarJuego);
+    }
+
+
+}
+
+function ganaUsuario() {
+    puntosUsuario++;
+    contenedorPuntosUsuario.innerText = puntosUsuario;
+    contenedorGanaPunto.innerText = "¡Ganaste un punto!"
+}
+
+function ganaPC() {
+    puntosPC++;
+    contenedorPuntosPC.innerText = puntosPC;
+    contenedorGanaPunto.innerText = "¡La computadora ganó un punto!"
+}
+
+function empate() {
+    contenedorGanaPunto.innerText = "¡Empate!"
+}
+
+function reiniciarJuego() {
+    reiniciar.classList.add("disabled");
+    elegiTuArma.classList.remove("disabled");
+    mensaje.classList.add("disabled");
+
+    puntosUsuario = 0;
+    puntosPC = 0;
+    
+    contenedorPuntosUsuario.innerText = puntosUsuario;
+    contenedorPuntosPC.innerText = puntosPC;
+
+    instrucciones.innerText = "El primero en llegar a 5 puntos gana."
+}
+
+function goBack(){
+    reglas.style.display = "none";
+    historia.style.display = "none";
+}
+
+function rules() {
+    reglas.style.display = "flex";
+}
+
+function history() {
+    historia.style.display = "flex";
+}
+
+goBack()
